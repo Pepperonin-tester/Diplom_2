d36b829cec0173a1a75124113c9a855b72dbad94
Dmitry Skvortsov <skvordmitry0811@yandex.ru>
2026-06-25 19:32:05 +0500

    Add fixtures: random user data, valid ingredient id, registered user

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import random
+import string
+import requests
+
+from constants import BASE_URL, REGISTER_ENDPOINT, INGREDIENTS_ENDPOINT, DELETE_USER_ENDPOINT
+
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase + string.digits
+    random_chars = random.choices(letters, k=length)
+    return ''.join(random_chars)
+
+@pytest.fixture
+def random_user_data():
+    user_data = {
+        "email": f"{generate_random_string(10)}@example.com",
+        "password": f"{generate_random_string(8)}",
+        "name": f"{generate_random_string(6)}"
+    }
+    return user_data
+
+@pytest.fixture
+def valid_ingredient_id():
+    response = requests.get(BASE_URL + INGREDIENTS_ENDPOINT)
+    data = response.json()
+    ingredients_list = data["ingredients"]
+    return ingredients_list[0]["_id"]
+
+@pytest.fixture
+def registered_user(random_user_data):
+    response = requests.post(BASE_URL + REGISTER_ENDPOINT, json=random_user_data)
+    data = response.json()
+    access_token = data["accessToken"]
+    yield data
+    requests.delete(BASE_URL + DELETE_USER_ENDPOINT, headers={"Authorization": access_token})
---
+++ constants.py
@@ -0,0 +1,19 @@
+
+
+# Базовый URL
+BASE_URL = "https://stellarburgers.education-services.ru/api"
+
+# Регистрация пользователя POST
+REGISTER_ENDPOINT = "/auth/register"
+
+# Авторизация пользователя POST
+LOGIN_ENDPOINT = "/auth/login"
+
+# Оформление заказа POST
+ORDER_ENDPOINT = "/orders"
+
+# Удаление пользователя DELETE
+DELETE_USER_ENDPOINT = "/auth/user"
+
+# Получение ингредиентов GET
+INGREDIENTS_ENDPOINT = "/ingredients"
